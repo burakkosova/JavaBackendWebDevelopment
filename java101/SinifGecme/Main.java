c2b10a4cf63c67d683068542fc8e8b0d688215e9
Burak Kosova <burak.kosova25@gmail.com>
2021-05-16 03:53:51 +0300

    java 101 ödev 10 sınıfı geçme

--- java101/SinifGecme/Main.java
+++ java101/SinifGecme/Main.java
@@ -7,39 +7,39 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int math, physics, chemistry, turkish, music,count=0;
-        double total=0.0, average;
+        double math, physics, chemistry, turkish, music,count=0, total=0.0, average;
+         
 
         System.out.print("Matematik notunuzu giriniz: ");
-        math = input.nextInt();
+        math = input.nextDouble();
         if(math <= 100 && math >= 0){
             total += math;
             count++;
         }
 
         System.out.print("Fizik notunuzu giriniz: ");
-        physics = input.nextInt();
+        physics = input.nextDouble();
         if(physics <= 100 && physics >= 0){
             total += physics;
             count++;
         }
 
         System.out.print("Kimya notunuzu giriniz: ");
-        chemistry = input.nextInt();
+        chemistry = input.nextDouble();
         if(chemistry <= 100 && chemistry >= 0) {
             total += chemistry;
             count++;
         }
 
         System.out.print("Türkçe notunuzu giriniz: ");
-        turkish = input.nextInt();
+        turkish = input.nextDouble();
         if(turkish <= 100 && turkish >= 0){
             total += turkish;
             count++;
         }
 
         System.out.print("Müzik notunuzu giriniz: ");
-        music = input.nextInt();
+        music = input.nextDouble();
         if(music <= 100 && music >= 0){
             total += music;
             count++;
