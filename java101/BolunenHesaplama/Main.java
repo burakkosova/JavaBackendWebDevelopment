1780876b0e785c87fe2c4877548c01ff9f06cb12
Burak Kosova <burak.kosova25@gmail.com>
2021-05-16 16:14:10 +0300

    java 101 ödev 17 bölünen hesaplama

---
+++ java101/BolunenHesaplama/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz : ");
+        int number = input.nextInt();
+        int total = 0, count = 0;
+         for (int i=1; i<=number; i++) {
+             if(i % 12 == 0){
+                 total += i;
+                 count++;
+             }
+         }
+        System.out.println("3 ve 4'e tam bölünen sayıların ortalaması : " + (double) total/count);
+    }
+}
